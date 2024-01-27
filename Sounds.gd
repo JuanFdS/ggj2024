ff55b18152af89bb872642c1d79378425e75b09a
lulucasg <69694915+lulucasg@users.noreply.github.com>
2024-01-27 17:04:11 -0300

    added stop_all_sounds func

--- Sounds.gd
+++ Sounds.gd
@@ -10,3 +10,12 @@ func play_gallina():
 
 func play_lamp_collision():
 	$LampCollisionSfx.play_random()
+
+func play_lamp_win():
+	pass
+
+func stop_all_sounds():
+	$LampWinSfx.stop()
+	var streams = $GallinaSfx.get_children()
+	for stream in streams:
+		stream.stop()
