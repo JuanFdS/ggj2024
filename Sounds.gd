0c2d8d6896e776d1d7a1ff99ef8d9fa851e3099f
lulucasg <69694915+lulucasg@users.noreply.github.com>
2024-01-27 06:05:50 -0300

    Fixed Gallinas Sound

--- Sounds.gd
+++ Sounds.gd
@@ -4,5 +4,6 @@ func play_spawn():
 	$SpawnSfx.play_random()
 
 func play_gallina():
-	$GallinaSfx.set_pitch_scale(randi_range(1,10))
-	$GallinaSfx.play_random()
+	var stream = $GallinaSfx.get_children().pick_random()
+	stream.set_pitch_scale(randf_range(0.85,1.5))
+	stream.play()
