c9ac02171e4cf4310898a9058d2ee491b560826c
Facundo Javier Gelatti <javiergelatti@gmail.com>
2024-01-27 04:43:19 -0300

    Arreglar error de tipeo

--- Sounds.gd
+++ Sounds.gd
@@ -4,5 +4,5 @@ func play_spawn():
 	$SpawnSfx.play_random()
 
 func play_gallina():
-	$GallinaSfx_set_pitch_scale(randi_range(1,10))
+	$GallinaSfx.set_pitch_scale(randi_range(1,10))
 	$GallinaSfx.play_random()
